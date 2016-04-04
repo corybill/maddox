3f2dac0b71ddb65032d6657d5fc53d5d068b564a
corybillparrish <corybillparrish@gmail.com>
2016-04-03 22:21:53 -0400

    [From Promise Scenario] - Finishes tests for From Promise Scenario.

---
+++ spec/testable/modules/test-module/from-promise-controller.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const Service = require("./test-module-service");
+
+class Controller {
+
+  static noProxies(req) {
+    return Service.executeWithNoProxies(req.params, req.query);
+  }
+
+  static statefulFactoryProxy(req) {
+    return Service.executeWithStatefulFactoryProxy(req.params, req.query);
+  }
+
+  static statefulSingletonProxy(req) {
+    return Service.executeWithStatefulSingletonProxy(req.params, req.query);
+  }
+
+  static statelessEs6Proxy(req) {
+    return Service.executeWithStatelessEs6Proxy(req.params, req.query);
+  }
+
+  static statelessPreEs6SingletonProxy(req) {
+    return Service.executeWithStatelessPreEs6SingletonProxy(req.params, req.query);
+  }
+
+  static statelessPreEs6StaticProxy(req) {
+    return Service.executeWithStatelessPreEs6StaticProxy(req.params, req.query);
+  }
+
+}
+
+module.exports = Controller;
